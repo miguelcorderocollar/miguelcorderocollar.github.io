d09ae56e3e46ec9241eaa7ffe9d152141a0186a7
Miguel Cordero Collar <mikelon797@gmail.com>
2020-04-16 18:09:59 +0200

    Navbar + I'm upgrade

--- js/scripts.js
+++ js/scripts.js
@@ -36,11 +36,31 @@ $('.navlinks').click(function () {
     $("#mainListDiv").fadeIn();
 });
 
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
 
 textSequence(0);
 
 function textSequence(i) {
-    var example = ['Curious', 'Easy-going', 'Honest', 'Industrial Engineer'];
+    var example = ['Curious', 'Honest', 'Industrial Engineer', 'Proactive', 'Data driven', 'Professional', 'Patient', 'Consistent', 'Reliable', 'Innovative', 'Resourceful', 'Commited', 'Respectful', 'Confident', 'Analytical', 'Observant', 'Passionate', 'Organised', 'Perceptive'];
+    example = shuffle(example)
     if (example.length > i) {
         setTimeout(function () {
             document.getElementById("sequence").innerHTML = example[i];
@@ -49,6 +69,23 @@ function textSequence(i) {
 
     } else if (example.length == i) { // Loop
         textSequence(0);
+    }
+};
+
+
+
+textSequence2(0);
+
+function textSequence2(i) {
+    var example2 = ['Soon.  ', 'Soon.. ', 'Soon...', 'Soon   '];
+    if (example2.length > i) {
+        setTimeout(function () {
+            document.getElementById("sequence2").innerHTML = example2[i];
+            textSequence2(++i);
+        }, 500); // 1 second (in milliseconds)
+
+    } else if (example2.length == i) { // Loop
+        textSequence2(0);
     }
 };
 
